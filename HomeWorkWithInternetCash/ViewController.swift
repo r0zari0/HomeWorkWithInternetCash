8d98e35588a0b6264c3a0603ede8549795be027f
Maksym <maksymstovolos2002@gmail.com>
2022-08-27 15:12:25 -0400

    Initial Commit

---
+++ HomeWorkWithInternetCash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkWithInternetCash
+//
+//  Created by Max Stovolos on 8/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
